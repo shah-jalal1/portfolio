ed1c5019b9a67a2e0710c3a966d905078d4864b2
shahjalalcse <shah-jalal1>
2021-05-02 15:51:42 +0600

    github linkdin added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,6 +8,8 @@ const Header = () => {
     return (
         <nav class="navbar navbar-expand-lg navbar-light  nav-section">
 
+            <h2 className="ml-5 text-white">Shah jalal</h2>
+
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
--- src/components/Home/Hero/Hero.js
+++ src/components/Home/Hero/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Hero.css'
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 const Hero = () => {
     return (
@@ -15,6 +16,11 @@ const Hero = () => {
           </p>
 
           <a href="https://drive.google.com/uc?export=download&id=1ss1i0nibS5sMuo4V9OHXPQmYU75mY81M"><button className="btn btn-success">Resume</button></a>
+
+          <div className="mt-4">
+            <a href="https://github.com/shah-jalal1" target="_blank"><FaGithub size={40}/></a>
+            <a href="https://www.linkedin.com/in/shah-jalal-10/" target="_blank" className="ml-4"><FaLinkedin size={40}/></a>
+          </div>
 
         </div>
       </div>
